7d978f939c73121fd3068b63a9866968f0907d87
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-11-01 06:52:36 -0600

    Finish homework 3 assignments

--- src/hw03/hw3.sql
+++ src/hw03/hw3.sql
@@ -5,7 +5,13 @@ SELECT name, statecode FROM states;
 SELECT name, statecode, population_1950, population_2010 FROM counties WHERE name LIKE 'Prince%';
 
 -- 3. Write a single query to list only the population in year 2010 for the state represented by Sen. Richard Lugar.
-SELECT name, population_2010 FROM states WHERE statecode=(SELECT statecode FROM senators WHERE name='Richard Lugar');
+SELECT population_2010 FROM states WHERE statecode=(SELECT statecode FROM senators WHERE name='Richard Lugar');
 
 -- 4. Write a single query to report only the total number of the counties in 'Maryland'. The query should not hard-code the state code for Maryland (join the two tables in the WHERE clause).
-SELECT COUNT(*) FROM counties INNER JOIN states ON statecode = statecode WHERE 
+SELECT COUNT(*) FROM counties INNER JOIN states ON counties.statecode = states.statecode WHERE states.name='Maryland';
+
+-- 5. Write a single query to find the name of the state that was admitted last into the union.
+SELECT name FROM states ORDER BY admitted_to_union DESC LIMIT 1;
+
+-- 6. Find all democratic (i.e., with affiliation = 'D') senators that are not chairman of any committee or subcommittee.
+SELECT name FROM senators WHERE affiliation='D' AND name NOT IN (SELECT chairman FROM committees);
